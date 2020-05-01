ac1622c349962679933a891289c2fa6c97faa86a
IsabelNoguera9 <50124066+IsabelNoguera9@users.noreply.github.com>
2020-05-01 19:41:48 -0400

    I started the bot
    e

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const core = require('core.js');
+const client = new Discord.Client();
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('message', function(message) {
+	console.log(message.content);
+  message.content = message.content.lowercase
+  if (message.content === '~creators') {
+	message.channel.send('My creators are Daniel and Isabel Noguera. Daniel is a junior and Isabel is in 8th Grade at Hamilton Virtual School. Daniel has been coding for a while and Isabel is newer to the coding world. ');
+}
+if (message.content === '~help') {
+message.channel.send('I am a Discord IDE bot. I turn commands into code that you can run. For a full command list, say “~fullcommands”. Have Fun!');
+}
+
+});
