9b969f8f2427b8827b2578a9f02584f2a292a7f9
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-05-01 19:43:19 -0400

    quick fixes to bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
-const core = require('core.js');
+const core = require('./core.js');
 const client = new Discord.Client();
 client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message', function(message) {
 	console.log(message.content);
-  message.content = message.content.lowercase
+	message.content = message.content.toLowerCase()
   if (message.content === '~creators') {
 	message.channel.send('My creators are Daniel and Isabel Noguera. Daniel is a junior and Isabel is in 8th Grade at Hamilton Virtual School. Daniel has been coding for a while and Isabel is newer to the coding world. ');
 }
