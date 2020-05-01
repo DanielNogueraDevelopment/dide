420fa22318ced9383fd403c7cbc88a09542dc578
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-05-01 19:23:18 -0400

    updated core.js

--- core.js
+++ core.js
@@ -1,10 +1,55 @@
 const express = require("express");
-const mongoose = require(mongoose);
-
+const mongoose = require("mongoose");
+const config = require("./config")
+//requiring
 core = {};
 
 
 
+//database config
+mongoose.connect(config.mongo, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("mongo connected")).catch(err => console.log(err))
+var codeschema = new mongoose.Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    title: { type: String },
+    content: {type:String}
+})
+var Code = mongoose.model("Code", codeschema)
 
 
-module.exports=core
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = core;
