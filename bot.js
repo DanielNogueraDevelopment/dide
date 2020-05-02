aa8d351f037c15e244d96942bf69f2e8549675f4
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-05-01 20:59:40 -0400

    Added run function

--- bot.js
+++ bot.js
@@ -1,6 +1,60 @@
 const Discord = require('discord.js');
 const core = require('./core.js');
 const client = new Discord.Client();
+const mongoose = require("mongoose")
+
+
+
+
+var codeschema = new mongoose.Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    title: { type: String },
+    content: { type: String }
+})
+var Code = mongoose.model("Code", codeschema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -15,3 +69,24 @@ message.channel.send('I am a Discord IDE bot. I turn commands into code that you
 }
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+//this code will grab a value
+/*core.Code.findOne({ title: name }).then(function (code) {
+    console.log(code.content)
+    if (code) {
+        return code.content;
+    } else {
+        return false;
+    }
+})*/
--- core.js
+++ core.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("./config")
+const Interpreter = require("js-interpreter")
 //requiring
 core = {};
 
@@ -26,6 +27,42 @@ core.set = function (name, value) {
             console.log(err)
         }
     })
+    return true;
+}
+
+core.get = function (name) {
+    Code.findOne({ title: name }).then(function (code) {
+        console.log(code.content)
+        if (code) {
+            return code.content;
+        } else {
+            return false;
+        }
+    })
+}
+
+core.run = function (code) {
+    try {
+        var sandbox = new Interpreter(code);
+        var running = true;
+        while (running) {
+            try {
+                
+                if (sandbox.step() == false) {
+                    running=false
+                }
+            } catch(e){ running = false; throw e}
+        }
+        if (sandbox.value.length > 100) {
+            return "maximum return length is 100!"
+        } else {
+            return sandbox.value;
+        }
+
+
+
+
+    } catch(e){ return e}
 }
 
 
@@ -39,11 +76,8 @@ core.set = function (name, value) {
 
 
 
-
-
-
 //database connect
-mongoose.connect(config.mongo, { useNewUrlParser: true, useUnifiedTopology: true }).then(function () { console.log("mongo connected"); console.log("setting.."); core.set("daniel", "e") }).catch(err => console.log(err))
+mongoose.connect(config.mongo, { useNewUrlParser: true, useUnifiedTopology: true }).then(function () { console.log("mongo connected");}).catch(err => console.log(err))
 var codeschema = new mongoose.Schema({
     date: {
         type: Date,
