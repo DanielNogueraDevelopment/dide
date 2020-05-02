b37087a82e9625b8092e6c602bafe73c40bbbf77
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-05-01 22:18:55 -0400

    Added view and viewspecific to core.js

--- core.js
+++ core.js
@@ -22,11 +22,19 @@ var Code = mongoose.model("Code", codeschema)
 
 //core methods
 core.set = function (name, value) {
-    new Code({ title: name, content: value }).save(function (err) {
-        if (err) {
-            console.log(err)
+    Code.findOne({ title: name }).then(function (code) {
+        if (code) {
+            code.content = value
+            new Code(code).save()
+        } else {
+            new Code({ title: name, content: value }).save(function (err) {
+                if (err) {
+                    console.log(err)
+                }
+            })
         }
     })
+
     return true;
 }
 
@@ -65,6 +73,48 @@ core.run = function (code) {
     } catch(e){ return e}
 }
 
+core.add = function (name, newcode) {
+    Code.findOne({ title: name }).then(function (code) {
+        if (code) {
+            new Code({ title: code.title, date: code.date, content: (code.content + newcode) }).save()
+        } else {
+            return false;
+        }
+    })
+}
+
+core.view = function (name) {
+    Code.findOne({ title: name }).then(function (code) {
+        if (code) {
+            code.content = code.content.split(/\r?\n/);
+            returner=""
+            for (i = 0; i < code.content.length; i++) {
+                returner=returner
+                    +i+" "+code.content[i]+"\n"
+            }
+            console.log(returner);
+        } else {
+            return "could not view"
+        }
+    })
+}
+
+core.viewspecific = function (name, start,end) {
+    Code.findOne({ title: name }).then(function (code) {
+        if (code) {
+            code.content = code.content.split(/\r?\n/);
+            returner = ""
+            for (i = start; i <= end; i++) {
+                returner = returner
+                    + i + " " + code.content[i] + "\n"
+            }
+            console.log(returner);
+        } else {
+            return "could not view"
+        }
+    })
+}
+
 
 
 
@@ -77,7 +127,7 @@ core.run = function (code) {
 
 
 //database connect
-mongoose.connect(config.mongo, { useNewUrlParser: true, useUnifiedTopology: true }).then(function () { console.log("mongo connected");}).catch(err => console.log(err))
+mongoose.connect(config.mongo, { useNewUrlParser: true, useUnifiedTopology: true }).then(function () { console.log("mongo connected");core.viewspecific("daniel",0,3)}).catch(err => console.log(err))
 var codeschema = new mongoose.Schema({
     date: {
         type: Date,
